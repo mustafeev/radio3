c951c1329ae785bffbbeb4490cc6b6d14a8a2650
Vadim Mustafeev <ya.mystafeev@yandex.ru>
2022-12-02 20:28:17 +0400

    secondary commit

--- src/test/java/Radio3Test.java
+++ src/test/java/Radio3Test.java
@@ -3,8 +3,8 @@ import org.junit.jupiter.api.Test;
 
 public class Radio3Test {
 
-    public class Radio2Test {
-        public class radio2Test {
+    public class radio3Test {
+        public class radio3Test {
             @Test
             public void test() {
                 int stationsCount;
@@ -16,4 +16,27 @@ public class Radio3Test {
             }
         }
     }
+    @Test
+    public void next() {
+        Radio3 radio = new Radio3();
+        radio.setCurrentStation(5);
+
+        radio.next();
+
+        int expected = 6;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void prev() {
+        Radio3 radio = new Radio3();
+        radio.setCurrentStation(0);
+
+        radio.prev();
+
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
